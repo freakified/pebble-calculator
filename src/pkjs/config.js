6d17aa23caf9dd2c41eeb7ca466ea8034e349ec6
Dan Tilden <freakified@gmail.com>
2026-05-02 21:36:15 +0900

    add branding

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -13,9 +13,22 @@ module.exports = [
       {
         "type": "toggle",
         "messageKey": "RPN_MODE",
-        "label": "RPN Mode",
+        "label": "Enable RPN Mode",
         "defaultValue": false,
-        "description": "Enable Reverse Polish Notation (RPN) mode with a 4-register stack (T/Z/Y/X). When off, the calculator uses standard infix notation."
+        "description": "If enabled, the calculator will use RPN mode with a 4-register stack."
+      },
+      {
+        "type": "text",
+        "defaultValue": "<strong>About RPN</strong><br>RPN stands for Reverse Polish Notation, and has been around for a while, though it's mostly favored by financial analysts. A key advantage of RPN is that operations occur in an entirely consistent manner, it does not require parenthesis, and allows you to fix your mistakes without wiping out your entire calculation!<br><br><strong>Let's learn RPN!</strong><br>To add 1 and 5, you would press 1, then ENTER, then 5, then PLUS. For more exciting recipes, check out this <a href='https://hansklav.home.xs4all.nl/rpn/'>interesting tutorial</a>!"
+      }
+    ]
+  },
+  {
+    "type": "section",
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "General"
       },
       {
         "type": "toggle",
@@ -31,3 +44,4 @@ module.exports = [
     "defaultValue": "Save Settings"
   }
 ];
+
